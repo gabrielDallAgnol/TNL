5d4c892fe672437dbc53941833393ea3b638ec15
Gabriel Schmit Dall Agnol <gabriel.dallagnol@southsystem.com.br>
2020-01-24 11:13:05 -0300

    feature: Progress Login Screen

---
+++ TNL/Extensions/UIViewControllerExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewControllerExtensions.swift
+//  TNL
+//
+//  Created by Gabriel Schmit Dall Agnol on 24/01/20.
+//  Copyright © 2020 Gabriel Schmit Dall Agnol. All rights reserved.
+//
+
+import Foundation
