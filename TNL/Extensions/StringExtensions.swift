8ddd1d7c7f2ae2026c477e8fa3a3d97586f706c4
Gabriel Schmit Dall Agnol <gabriel.dallagnol@southsystem.com.br>
2020-01-25 17:20:26 -0300

    feature: Add an Alert View

--- TNL/Extensions/StringExtensions.swift
+++ TNL/Extensions/StringExtensions.swift
@@ -6,14 +6,45 @@
 //  Copyright © 2020 Gabriel Schmit Dall Agnol. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 extension String {
   
-  func isValidEmail() -> Bool {
-      let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-
-      let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-      return emailPred.evaluate(with: self)
+  var html2AttributedString: NSAttributedString? {
+    do {
+      return try NSAttributedString(
+        data: Data(utf8),
+        options: [
+          .documentType: NSAttributedString.DocumentType.html,
+          .characterEncoding: String.Encoding.utf8.rawValue
+        ],
+        documentAttributes: nil
+      )
+    } catch {
+      print("error: ", error)
+      return nil
+    }
   }
+  
+  func isValidEmail() -> Bool {
+    let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+    return emailPred.evaluate(with: self)
+  }
+}
+
+extension NSMutableAttributedString {
+    func replaceFont(with font: UIFont) {
+        beginEditing()
+        self.enumerateAttribute(.font, in: NSRange(location: 0, length: self.length)) { (value, range, stop) in
+            if let f = value as? UIFont {
+                let ufd = f.fontDescriptor.withFamily(font.familyName).withSymbolicTraits(f.fontDescriptor.symbolicTraits)!
+                let newFont = UIFont(descriptor: ufd, size: font.pointSize)
+                removeAttribute(.font, range: range)
+                addAttribute(.font, value: newFont, range: range)
+            }
+        }
+        endEditing()
+    }
 }
